8c7fed474b87f9442bb5c0ed4706205b0dbc9740
Kun Akos <kunaakos@gmail.com>
2015-06-16 16:04:46 +0200

    fb og tags, fixed links

--- Gruntfile.js
+++ Gruntfile.js
@@ -211,7 +211,8 @@ module.exports = function (grunt) {
             '<%= config.dist %>/styles/{,*/}*.css',
             '<%= config.dist %>/images/{,*/}*.*',
             '<%= config.dist %>/styles/fonts/{,*/}*.*',
-            '<%= config.dist %>/*.{ico,png}'
+            '<%= config.dist %>/*.{ico,png}',
+            '!<%= config.dist %>/images/fb_og.png'
           ]
         }
       }
